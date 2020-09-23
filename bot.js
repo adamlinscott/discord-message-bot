757fea22f9a24aa033ebe10c9a564a3483a53b99
Adam Linscott <adamrlinscott@gmail.com>
2020-09-23 17:11:35 +0100

    working ping command

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
-import Discord from 'discord.io';
-import logger from 'winston';
+var Discord = require('discord.io');
+var logger = require('winston');
 var auth = require('./auth.json');
 
 //Logger settings
@@ -17,11 +17,25 @@ var bot = new Discord.Client({
 
 bot.on('ready', (event) => {
     logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+    logger.info('Logged in as: ' + bot.username + ' - (' + bot.id + ')');
 });
 
 bot.on('message', (user, userID, channelID, message, event) => {
-    
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+        args = args.splice(1);
+
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
 })
 
